e1404e4b3c6a8c306037d3e0e3a9df19358400fb
Titinite <thibault.leray@supdevinci-edu.fr>
2026-03-19 11:57:28 +0100

    feat: level-1

--- Assets/Content/Scripts/Cars/CarManager.cs
+++ Assets/Content/Scripts/Cars/CarManager.cs
@@ -9,13 +9,11 @@ public class CarManager : MonoBehaviour
 
     public float speed = 6f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         Instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
